7fc1aad5f992d237b3501ac61d7655e203d13f05
junjiezhujason <junjie.zhu.jason@gmail.com>
2016-05-26 16:54:11 -0700

    generate_pca_of_log_rpkm.py complete

--- preprocessing/generate_pca_of_log_rpkm.py
+++ preprocessing/generate_pca_of_log_rpkm.py
@@ -40,110 +40,106 @@ def get_tissues_to_cols(tissue_list):
         tissues_to_cols[tissue] = cols
     return tissues_to_cols
 
-def normalize_column(mtx_in):
+def normalize_columns(mtx_in):
+    mtx_out = np.zeros(mtx_in.shape)
     num_features = mtx_in.shape[1]
-    for i in range(0, num_features):
+    for i in range(num_features):
         col = mtx_in[:, i]
         col = col - np.mean(col)
         std_dev = np.std(col)
         if std_dev > 0:
             col = col / std_dev
-        gene_features[:, i] = col
-    col = col_in - np.mean(col_in)
-    std_dev = np.std(col)
-    if std_dev > 0:
-        col = col / std_dev
-    return col
+        mtx_out[:, i] = col
+    return mtx_out
 
 '''
-
 *********************
         Main
 *********************
 '''
 if __name__ == "__main__":
 
-    gene2go_file_path = '../data/gene2go.txt' # If file doesn't exist, then run gene2go = download_ncbi_associations()
-    # rpkm_file_path = '../../CS341_Data/transcript_rpkm_in_go_nonzero_exp.txt'
-    # rpkm_file_path = '../data/small_example_data/GO-0000578_pos.txt'
-    gene_count_file_path = '../data/supp_GO_term_gene_counts.txt'
-    biomart_file_path = '../data/biomart_ensembl_to_entrez.txt'
-    obo_file_path = '../data/go-basic.obo'
+    # --------------------------------------
+    # input and output files
+    # --------------------------------------
+    rpkm_file_path = '../data/small_example_data/small_transcript_rpkm_in_go_nonzero_exp.txt'
+    rpkm_file_out = '../data/small_example_data/small_log_norm_pca_transcript_rpkm_in_go_nonzero_exp.txt'
 
+    # --------------------------------------
     # load tissue information
+    # --------------------------------------
     tissues = get_tissue_list('../data/tissues.txt');
     tissues_to_cols = get_tissues_to_cols(tissues)
-
-   
     num_features = 0
     for tissue in tissues_to_cols:
         num_features += len(tissues_to_cols[tissue])
     print 'Loaded '+str(num_features)+' features. Number of tissues:' + str(len(tissues))
 
-    
-    rpkm_file_path = '../data/small_example_data/small_transcript_rpkm_in_go_nonzero_exp.txt'
+    # --------------------------------------
+    # load count data and meta information
+    # --------------------------------------
     n_header_lines = 1
-    # count number of genes
+    n_meta_fields = 4
+    # 1. count number of genes
     rpkm_file = open(rpkm_file_path)
     num_genes = 0
     for (i, line) in enumerate(rpkm_file):
         if i >= n_header_lines:
             num_genes += 1
     rpkm_file.close()
-    print 'Reading:' + rpkm_file_path
-    print 'Number of Genes: ' + str(num_genes)
-
-    # read the full rpkm matrix
+    print 'Reading: ' + rpkm_file_path
+    print 'Number of genes: ' + str(num_genes)
+    # 2. read the full rpkm matrix
     full_mtx = np.empty((num_genes, num_features))  # each row will be the feature profile for a given gene
-    gene_ids = ["ID"]*num_genes
-    # file_name = '../../CS341_Data/experiment_inputs/' + term + '_neg_0.txt'
+    gene_info = ["NULL"]*num_genes
     print 'Reading:' + rpkm_file_path
     n_pcomp = 5
     rpkm_file = open(rpkm_file_path)
     for (i, line) in enumerate(rpkm_file):
         vals = line.rstrip().split('\t')
-        if i < n_header_lines: # TODO: double-check the number of headers to ignore 
-            first_fields =vals[0:3]
+        if i < n_header_lines: 
+            first_fields = '\t'.join(vals[0:(n_meta_fields-1)]) # print header later
             continue
-        idx = i - n_header_lines
-        if (idx % 100 == 0):
+        idx = i - n_header_lines  # gene index 
+        if (idx % 1000 == 0):
             print '    ' + str(idx) + ' genes read'
-        gene_ids[idx] = vals[0]
-        exp_levels = vals[4:]
-        # Convert expression levels to log(level+1)
+        gene_info[idx] = '\t'.join(vals[0:(n_meta_fields-1)])
+        exp_levels = vals[n_meta_fields:]
         exp_levels = [np.log10(float(exp_level)+1.0) for exp_level in exp_levels]
-        # full_mtx = np.append(full_mtx, [exp_levels], axis=0)
         full_mtx[idx,:] = exp_levels 
     rpkm_file.close()
-    print 'Complete!'
-    print 'Dimension of full input matrix: ' + str(full_mtx.shape)
+    print 'Completed reading full matrix. Dimensions: ' + str(full_mtx.shape)
 
+    # --------------------------------------
     # perform pca for each tissue
+    # --------------------------------------
     pca_tissues_to_cols = {}
     reduced_mtx = np.zeros([full_mtx.shape[0],n_pcomp*len(tissues)]);
     # reduced_cols= np.chararray(n_pcomp*len(tissues));
     reduced_cols= ["NULL"]*(n_pcomp*len(tissues));
-
     for idx, tissue in enumerate(tissues):
         print 'Reducing features for ' + tissue 
         pca_tissues_to_cols[tissue] = range(n_pcomp*idx, n_pcomp*(idx+1))
-        tissue_mtx = full_mtx[:,tissues_to_cols[tissue]]
+        tissue_mtx = normalize_columns(full_mtx[:,tissues_to_cols[tissue]])  # normalize tissue matrix
         assert(tissue_mtx.shape[1] >= n_pcomp)
-        # NORMALIZATION
-        tissue_mtx = normalize_columns(tissue_mtx)  
-
-
         pca = PCA(n_components=n_pcomp)
         reduced_tissue_mtx = pca.fit_transform(tissue_mtx)
         reduced_mtx[:,pca_tissues_to_cols[tissue]] = reduced_tissue_mtx
         for t_idx in pca_tissues_to_cols[tissue]:
             reduced_cols[t_idx] = tissue
-    print '*Data matrix size: ', str(reduced_mtx.shape)
-    # PANDA data frame
-    # reduced_data = pd.DataFrame(data=reduced_mtx,index=gene_ids,columns=reduced_cols)
-    first_row = first_fields + reduced_cols
-    print first_row
-    # print reduced_mtx
-    # print reduced_cols
+    print 'Reduced matrix size: ', str(reduced_mtx.shape)
 
-    # TODO: save reduced matrix to file as a dataframe to file
+    # --------------------------------------
+    # save reduced matrix to file 
+    # --------------------------------------
+    with open(rpkm_file_out , mode='wt') as myfile: 
+        myfile.write(first_fields)
+        myfile.write('\t')
+        myfile.write('\t'.join(reduced_cols))
+        myfile.write('\n')
+        for idx in range(len(gene_info)):
+            myfile.write(gene_info[idx])
+            myfile.write('\t')
+            myfile.write('\t'.join(map(str,reduced_mtx[:,idx])))
+            myfile.write('\n')
+
